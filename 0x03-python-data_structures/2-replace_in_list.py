e6ba2c6d38e3ac6409d621fce9fb51c30dbc75a2
temikiki <temitayokiki@gmail.com>
2022-08-29 15:21:15 -0700

    two

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list[idx]
+    elif idx >= len(my_list):
+        return my_list[idx]
+    else:
+        elment = my_list[idx]
+        return my_list[idx]
