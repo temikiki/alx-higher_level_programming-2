d157e418ccd28110044c7d044acb82bfc7b50a9f
temikiki <temitayokiki@gmail.com>
2022-08-29 15:23:39 -0700

    two

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,5 +5,5 @@ def replace_in_list(my_list, idx, element):
     elif idx >= len(my_list):
         return my_list[idx]
     else:
-        elment = my_list[idx]
-        return my_list[idx]
+        elment = my_list[:]
+        return my_list[:]
