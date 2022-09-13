39c00a1b1ee1386e70dcec69fe403e770505dfb7
temikiki <temitayokiki@gmail.com>
2022-09-12 19:23:42 -0700

    two

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             line += 1
-        except (ValueError, TypeError):
+        except (ValueError, IndexError):
             continue
         print("")
         return (line)
