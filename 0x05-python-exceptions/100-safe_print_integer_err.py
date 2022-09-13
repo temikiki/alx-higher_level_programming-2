3a333444fc9dc530d6bbf8397847f8c3039e437f
temikiki <temitayokiki@gmail.com>
2022-09-12 20:21:07 -0700

    going

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
-    line = 0
+    
     try:
         print("{:d}".format(value))
         return True
-    except TypeError:
+    except (TypeError, ValueError):
         print("Exception: {}".format(sy.exc_info()[1]), file=sys.stderr)
         return (False)
 
