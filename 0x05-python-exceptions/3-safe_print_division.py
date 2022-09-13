d15190af868b92cca066177b9cf2c75ab5b60a27
temikiki <temitayokiki@gmail.com>
2022-09-12 19:43:50 -0700

    three

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except (ZeroDivisionError, TypeError):
         result = none
     finally:
-        print("inside results: {}".format(result))
+        print("inside result: {}".format(result))
     return (result)
