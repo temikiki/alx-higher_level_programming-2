f9cbc5c87c78aebe81d3d8bc08818bd170f12a79
temikiki <temitayokiki@gmail.com>
2022-09-12 20:29:58 -0700

    going

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    result = 0
+    for n in range(1, 3):
+        try:
+            if n > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / n
+        except Exception:
+            result = a + b
+            break
+    return (result)
